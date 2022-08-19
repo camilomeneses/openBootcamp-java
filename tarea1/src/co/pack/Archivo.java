9cdd8a28e64a3aadaee39957d4a24d4b27d5a72f
Camilo Meneses <kamilomeneses@gmail.com>
2022-08-18 19:35:37 -0500

    Task1 Finish

---
+++ tarea1/src/co/pack/Archivo.java
@@ -0,0 +1,12 @@
+package co.pack;
+
+public class Archivo {
+  public static void main( String[] args ) {
+    int numero = 1;
+    long numeroLong = 1L;
+    double numeroDouble = 1.0;
+    boolean flag = true;
+    String saludo = "Hola OpenBootcamp";
+    System.out.printf( "Datos a imprimir: int: %s, long: %s, double: %s, boolean: %s, String: %s %n", numero, numeroLong, numeroDouble, flag, saludo );
+  }
+}
